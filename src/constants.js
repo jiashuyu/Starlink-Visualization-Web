df7c0c77dd02a59858a585e47d6e55aa4f59125e
Shuyu Jia <shuyuj2@illinois.edu>
2022-10-10 17:25:52 +0800

    My first commit

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const SAT_API_KEY = "4CVY5B-E6XS6L-7G3EVW-4XPK";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const BASE_URL = "https://54.193.42.77"; // your backend endpoint when deploy
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+
+export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
